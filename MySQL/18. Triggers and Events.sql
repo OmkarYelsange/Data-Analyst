d25454ad76224fcdea12cc88c419492432b00044
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-06-16 12:06:00 +0530

    Completed Triggers

--- MySQL/18. Triggers and Events.sql
+++ MySQL/18. Triggers and Events.sql
@@ -5,3 +5,21 @@ FROM employee_demographics;
 
 SELECT * 
 FROM employee_salary;
+
+
+DELIMITER $$
+CREATE TRIGGER employee_insert
+	AFTER INSERT ON employee_salary
+    FOR EACH ROW 
+BEGIN
+	INSERT INTO employee_demographics (employee_id, first_name, last_name)
+    VALUES (NEW.employee_id, NEW.first_name, NEW.last_name);
+END $$
+DELIMITER ;
+
+INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+VALUES (13, 'John', 'Deh', 'GM', 100000, 4)
+
+
+
+# EVENTS
