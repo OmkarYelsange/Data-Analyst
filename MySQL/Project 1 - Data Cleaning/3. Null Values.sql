0ecd591cc998ea7833c07194b7e90c13f9b77354
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-06-20 11:56:01 +0530

    Completed Null values operation

--- MySQL/Project 1 - Data Cleaning/3. Null Values.sql
+++ MySQL/Project 1 - Data Cleaning/3. Null Values.sql
@@ -3,4 +3,34 @@
 
 SELECT * 
 FROM layoffs_staging2
-WHERE total_laid_off = 0;
+WHERE total_laid_off = 0
+AND percentage_laid_off = 0;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+UPDATE layoffs_staging2
+SET industry = NULL 
+WHERE industry = '';
+
+SELECT * 
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+OR total_laid_off = '';
+
+UPDATE layoffs_staging2
+SET total_laid_off = 0  
+WHERE total_laid_off = '';
+
+SELECT funds_raised
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET funds_raised = 0  
+WHERE funds_raised = '';
+
