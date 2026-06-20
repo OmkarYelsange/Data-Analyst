564807d13a94fea3ca708af0cd712b37f7760314
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-06-21 01:23:52 +0530

    Started with Project 2 - EDA

--- MySQL/Project 2 - Exploratory Data Analysis/1. Exploratory Data Analysis.sql
+++ MySQL/Project 2 - Exploratory Data Analysis/1. Exploratory Data Analysis.sql
@@ -3,6 +3,14 @@
 SELECT *
 FROM layoffs_staging2;
 
+SELECT *
+FROM layoffs_staging2
+WHERE country = '';
+
+UPDATE layoffs_staging2
+SET country = 'Unknown'
+WHERE country = '';
+
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN total_laid_off INT;
 
@@ -22,3 +30,35 @@ FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 2 DESC;
+
+SELECT stage, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+SELECT company, SUM(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT company, AVG(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
