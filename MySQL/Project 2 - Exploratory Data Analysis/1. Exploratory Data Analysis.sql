a2afa2ec7394b3882753a8d76bf07ec271724003
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-06-21 22:26:47 +0530

    Completed Prject 2 - EDA Successfully

--- MySQL/Project 2 - Exploratory Data Analysis/1. Exploratory Data Analysis.sql
+++ MySQL/Project 2 - Exploratory Data Analysis/1. Exploratory Data Analysis.sql
@@ -62,3 +62,45 @@ SELECT company, AVG(percentage_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
+
+# ROLLING TOTAL LAYOFFS BY DATES
+SELECT `date`, MONTH(`date`)
+FROM layoffs_staging2;
+
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY `MONTH`
+ORDER BY 1;
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+GROUP BY `MONTH`
+ORDER BY 1
+)
+SELECT `MONTH`, 
+total_off, 
+SUM(total_off) OVER (ORDER BY `MONTH`) AS rolling_total
+FROM Rolling_Total;
+
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY company;
+
+WITH Company_Year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), Company_Year_Rank AS
+(
+SELECT * ,
+DENSE_RANK() OVER ( PARTITION BY years ORDER BY total_laid_off DESC ) AS Ranking
+FROM Company_Year
+)
+SELECT *
+FROM Company_Year_Rank
+WHERE Ranking <= 5;
