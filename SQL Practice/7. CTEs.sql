3041336c554cfb84e83b3a8001a66f1b7f92fd3f
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-09-20 23:38:03 +0530

    Practiceing SQL

--- SQL Practice/7. CTEs.sql
+++ SQL Practice/7. CTEs.sql
@@ -4,7 +4,9 @@
 
 -- Q43 Using a CTE, calculate total sales by category and return the top 3 categories.
 
+
 -- Q44 Using a CTE, find the customers who have placed at least two orders.
+
 
 -- Q45 Using multiple CTEs, calculate:
 -- customer_name
