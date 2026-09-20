d0dbfbc742862fa9576807294ae7b23fb3a02035
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-09-20 23:37:43 +0530

    Practiceing SQL

--- SQL Practice/7. CTEs.sql
+++ SQL Practice/7. CTEs.sql
@@ -1,6 +1,7 @@
 -- 🔴 LEVEL 6 — CTEs
 -- Q42 Using a CTE, calculate total spending per customer and return customers whose spending is greater than ₹20,000.
 
+
 -- Q43 Using a CTE, calculate total sales by category and return the top 3 categories.
 
 -- Q44 Using a CTE, find the customers who have placed at least two orders.
