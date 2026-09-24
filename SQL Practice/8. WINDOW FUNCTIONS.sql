cedc846439d3a369da96536e40a8a4311b87b736
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-09-24 15:01:32 +0530

    Practicing SQL

--- SQL Practice/8. WINDOW FUNCTIONS.sql
+++ SQL Practice/8. WINDOW FUNCTIONS.sql
@@ -50,7 +50,25 @@ WHERE price_rank = 1;
 
 -- Q49 Find the second-highest-priced product in every category.
 -- This is an excellent interview question.
-
+WITH RankedProducts AS (
+    SELECT 
+        category,
+        product_id,
+        product_name,
+        price,
+        DENSE_RANK() OVER (
+            PARTITION BY category 
+            ORDER BY price DESC
+        ) AS price_rank
+    FROM products
+)
+SELECT 
+	product_id,
+    product_name,
+    category,
+    price
+FROM RankedProducts
+WHERE price_rank = 2;
 
 -- Q50 Assign a sequential row number to every order based on order_date.
 -- Use:
